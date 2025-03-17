0428ad1a5f6cb261604ed597792a0322a12f938e
MoustafaHassan16 <m.hassan31@wlv.ac.uk>
2025-03-17 14:14:29 +0000

    TaskController

---
+++ app/Controllers/TaskController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controllers;
+use App\Models\TaskModel;
+use CodeIgniter\Controller;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $taskModel = new TaskModel();
+        $tasks = $taskModel->where('user_id', session()->get('user_id'))->findAll();
+        return view('tasks/index', ['tasks' => $tasks]);
+    }
+
+    public function create()
+    {
+        return view('tasks/create');
+    }
+
+    
+    public function store()
+{
+    $taskModel = new TaskModel();
+    $data = json_decode($this->request->getBody(), true);
+    $data['user_id'] = session()->get('user_id');
+
+    if ($taskModel->save($data)) {
+        return $this->response->setJSON(['success' => true]);
+       
+    } else {
+        return $this->response->setJSON(['success' => false]);
+    }
+}
+
+public function edit($id)
+{
+    $taskModel = new TaskModel();
+    $task = $taskModel->find($id);
+    return view('tasks/edit', ['task' => $task]);
+}
+
+
+
+public function update($id)
+{
+    $taskModel = new TaskModel();
+    $data = [
+        'title' => $this->request->getPost('title'),
+        'description' => $this->request->getPost('description'),
+        'due_date' => $this->request->getPost('due_date'),
+        
+    ];
+    $taskModel->update($id, $data);
+    return redirect()->to('/dashboard');
+}
+
+
+public function delete($id)
+{
+    if ($this->request->isAJAX()) { 
+        $taskModel = new TaskModel();
+
+        
+        $task = $taskModel->where('id', $id)->where('user_id', session()->get('user_id'))->first();
+
+       
+            $taskModel->delete($id);
+
+            return $this->response->setJSON(['success' => true, 'message' => 'Task deleted successfully']);
+        } else {
+            return $this->response->setJSON(['success' => false, 'message' => 'Task not found or unauthorized']);
+        }
+    }
+
+}
