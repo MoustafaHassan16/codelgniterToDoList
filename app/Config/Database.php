411fc6b68258dfa432d05f091b30527874fc3492
MoustafaHassan16 <m.hassan31@wlv.ac.uk>
2025-03-17 14:00:54 +0000

    Updated DataBase

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'task-app',
+        'database'     => 'todolist',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
