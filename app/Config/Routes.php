0b3a67d7661c5e8e6952d659013b199d8ec92462
MoustafaHassan16 <m.hassan31@wlv.ac.uk>
2025-03-19 15:05:27 +0000

    Added the edit and delete routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,3 +20,8 @@ $routes->get('/dashboard', 'TaskController::index');
 $routes->get('/tasks/create', 'TaskController::create');
 
 $routes->post('/tasks/store', 'TaskController::store');
+
+$routes->get('/tasks/edit/(:num)', 'TaskController::edit/$1');
+$routes->post('/tasks/update/(:num)', 'TaskController::update/$1');
+
+$routes->delete('/tasks/delete/(:num)', 'TaskController::delete/$1');
