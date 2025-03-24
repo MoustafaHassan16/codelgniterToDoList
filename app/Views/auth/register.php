20b187c56a2a53c2cb7384330f232ad0c36bbba7
MoustafaHassan16 <m.hassan31@wlv.ac.uk>
2025-03-24 15:31:34 +0000

    added bootsrap and css to register

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -1,6 +1,31 @@
-<form action="<?= base_url('register') ?>" method="post">
-    <input type="text" name="name" placeholder="Full Name" required>
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <button type="submit">Register</button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Register</title>
+</head>
+<body>
+<div class="container-sm">
+<div class="control">
+<div class="cardstyle">
+    <form id="loginForm" action="<?= base_url('register') ?>" method="post">
+    <input class="form-control" type="text" name="name" placeholder="Full Name" required>
+    <input class="form-control" type="email" name="email" placeholder="Email" required>
+    <input class="form-control" type="password" name="password" placeholder="Password" required>
+    <button class="btn btn-primary" type="submit">Register</button>
 </form>
+<span>Already have an account? <button type="button" class="button-85"  onclick= "document.location='login'">Log In</button>
+</div>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+
