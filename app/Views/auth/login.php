5f34190b8d049797f13ff05250bc97c4aec160df
MoustafaHassan16 <m.hassan31@wlv.ac.uk>
2025-03-24 15:31:50 +0000

    form shake effect

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -12,7 +12,7 @@
 </header>
 <body>
 <div class="container-sm">
-<div class="control">
+<div id="shake" class="control">
 <div class="cardstyle">
 <h5 class="card-title text-center">Login</h5>
 <form id="loginForm">
@@ -20,7 +20,7 @@
     <input class="form-control" type="password" id="password" name="password" placeholder="Password" required>
     <button  class="btn btn-primary" type="submit">Login</button>
 </form>
-<span>Don't have an account? <button type="button" class="button-85"  onclick= "document.location='register'">Register</button>
+<span>Don't have an account? <button type="button" class="button-85"  onclick= "document.location='register'">Register</button></span>
 </div>
 </div>
 </div>
@@ -30,13 +30,14 @@
 
 
 <Script>
+let shakeDiv = document.getElementById('shake');
+
 document.getElementById("loginForm").addEventListener("submit", function (e) {
     e.preventDefault(); 
 
     let emailField = document.getElementById("email");
     let passwordField = document.getElementById("password");
     let alertmessage = document.getElementById("alert")
-   
     let formData = new FormData(this);
 
     fetch('<?= base_url('login') ?>',  {
@@ -47,8 +48,11 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
     .then(data => {
         if (data.status === "success") {
             window.location.href = data.redirect;
-        } else {
-            
+        } else {      
+            shakeDiv.style.marginLeft = '8px';
+            setTimeout(() => { shakeDiv.style.marginLeft = '0px'; }, 100);
+            setTimeout(() => { shakeDiv.style.marginLeft = '8px'; }, 200);
+            setTimeout(() => { shakeDiv.style.marginLeft = '0px'; }, 300);
             emailField.style.border = "2px solid red";
             passwordField.style.border = "2px solid red";
             alertmessage.style.display="block";
@@ -62,6 +66,7 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
     })
     .catch(error => console.error("Error:", error));
 });
+
 </Script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
