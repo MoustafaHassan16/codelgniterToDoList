e9e8cb9c3594fcf9a929af35ec224b60bef8d0a5
MoustafaHassan16 <m.hassan31@wlv.ac.uk>
2025-03-25 11:46:39 +0000

    Hid my Api key

--- app/Config/OpenAI.php
+++ app/Config/OpenAI.php
@@ -3,6 +3,6 @@
 namespace App\Config;
 
 class OpenAI
-{
-    public static $apiKey = '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';
-}
+ {
+//  
+ }
