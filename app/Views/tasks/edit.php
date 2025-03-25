043fa7ab942d01ae503bb008b317d763856bd67c
MoustafaHassan16 <m.hassan31@wlv.ac.uk>
2025-03-25 14:11:31 +0000

    added css to edit page

--- app/Views/tasks/edit.php
+++ app/Views/tasks/edit.php
@@ -1,24 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Task</title>
-</head>
-<body>
-    <h2>Edit Task</h2>
-    <form action="<?= base_url('tasks/update/' . $task['id']) ?>" method="post">
-        <label for="title">Title:</label>
-        <input type="text" name="title" id="title" value="<?= esc($task['title']) ?>" required>
-        
-        <label for="description">Description:</label>
-        <textarea name="description" id="description" required><?= esc($task['description']) ?></textarea>
-        
-        <label for="due_date">Due Date:</label>
-        <input type="date" name="due_date" id="due_date" value="<?= esc($task['due_date']) ?>" required>
-                
-        <button type="submit">Update Task</button>
-        <a href="<?= base_url('dashboard') ?>">Back to Dashboard</a>
-    </form>
-</body>
-</html>
+<script src="https://kit.fontawesome.com/881cf5052a.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="<?= base_url('css/edit.css') ?>">
+
+<style>.bootstrap-iso .formden_header h2, .bootstrap-iso .formden_header p, .bootstrap-iso form{font-family: Arial, Helvetica, sans-serif; color: black}.bootstrap-iso form button, .bootstrap-iso form button:hover{color: white !important;} .asteriskField{color: red;}</style>
+<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="<?= base_url('dashboard') ?>">Dashboard</a>
+    <i class="fa-solid fa-house-user"></i>
+  </div>
+</nav>
+
+<div class="container-lg">
+<div class="cardstyle">
+<div class="bootstrap-iso">
+ <div class="container-fluid">
+  <div class="row">
+   <div class="col-md-6 col-sm-6 col-xs-12">
+   <form class="adjust" action="<?= base_url('tasks/update/' . $task['id']) ?>" method="post">
+     <div class="form-group ">
+      <label class="control-label " for="name">
+       Title
+      </label>
+      <input  class="form-control" type="text" name="title" id="title" placeholder="Enter title" value="<?= esc($task['title']) ?>" required>
+     </div>
+     <div class="form-group ">
+      <label class="control-label " for="textarea">
+       Description
+      </label>
+      <textarea class="form-control" name="description" id="description" placeholder="Enter description" required><?= esc($task['description']) ?></textarea>
+     </div>
+     <div class="form-group ">
+      <label class="control-label " for="date">
+       Date
+      </label>
+      <div class="input-group">
+
+        <input class="form-control" type="date" name="due_date" id="due_date" value="<?= esc($task['due_date']) ?>">
+      </div>
+     </div>
+     <div class="form-group">
+      <div>
+       <button class="btn btn-primary " name="submit" type="submit">
+        Submit
+       </button>
+        </div>
+       </div>
+      </form>
+     </div>
+    </div>
+   </div>
+  </div>
+ </div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
